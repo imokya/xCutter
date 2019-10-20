e8042efb7c79e10b1cc71379bb654e90d14974b0
imokya <876657294@qq.com>
2019-10-20 14:12:55 +0800

    first commit

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra')
 const path = require('path')
-
+const sharp = require('sharp')
 
 let des 
 
@@ -8,8 +8,6 @@ const builder = {
   async build(_des) {
 
   },
-  
-  
 }
 
 module.exports = builder
